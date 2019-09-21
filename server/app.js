32a357705b9e850c94bbaae513389a10932c2363
camila <k.stenico@gmail.com>
2019-09-21 15:55:42 -0300

    capivara

--- server/app.js
+++ server/app.js
@@ -7,11 +7,15 @@ const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const newsRoute = require('./routes/newsRoutes');
+const wppRoute = require('./routes/wppRoutes');
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', index);
 app.use('/news', newsRoute);
+app.use('/wpp', wppRoute);
+
 
 module.exports = app;
